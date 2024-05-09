18c6240bb2b2d970c14ede0e23b0dbbf6a7a51c4
sangamrajsh <sangamrajsh@yahoo.com>
2024-05-09 07:19:56 +0200

    Updates

---
+++ precache-manifest.61e4c50cb0992b33c0cd108584b4fdee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2e7c319e754ac8ba0ef56085af1831",
+    "url": "/react-dvc/index.html"
+  },
+  {
+    "revision": "113a0115463e41362f2e",
+    "url": "/react-dvc/static/css/main.2edf07d6.chunk.css"
+  },
+  {
+    "revision": "e5db2d670436e9f2b3e7",
+    "url": "/react-dvc/static/js/2.55c97d52.chunk.js"
+  },
+  {
+    "revision": "113a0115463e41362f2e",
+    "url": "/react-dvc/static/js/main.82cea4ec.chunk.js"
+  },
+  {
+    "revision": "148406126a3e29fce977",
+    "url": "/react-dvc/static/js/runtime~main.8fba3bd0.js"
+  },
+  {
+    "revision": "fb81867ff4f1d71bf634be2424154ebb",
+    "url": "/react-dvc/static/media/rajesh.fb81867f.webp"
+  }
+]);
