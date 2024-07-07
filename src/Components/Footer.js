1663d5e4e3bd3a5cb5fd75f84fa6e2b3a2eca719
Rajesh Sharma <32365748+sangamrajsh@users.noreply.github.com>
2024-07-07 14:13:17 +0200

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer(){
         <footer>
             <a href="https://twitter.com/sangamrajsh" alt="Twitter"><i className="fa fa-twitter-square fa-2x"></i></a>
             <a href="https://facebook.com/sangamrajsh" alt="Facebook"><i className="fa fa-facebook-square fa-2x"></i></a>
-            <a href="https://github.com/sangamrajsh" alt="Github"><i className="fa fa-github-square fa-2x"></i></a>
+            <a href="https://sangamrajsh.github.io/" alt="Github"><i className="fa fa-github-square fa-2x"></i></a>
             
         </footer>
     )
