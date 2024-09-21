dd3bfc90bac796ee0cf0eaff788ed0397dc0ddbc
Rajesh Sharma <32365748+sangamrajsh@users.noreply.github.com>
2024-09-21 21:44:59 +0200

    Update Name.js

--- src/Components/Name.js
+++ src/Components/Name.js
@@ -4,7 +4,7 @@ export default function Name(){
         <section>
         <h1 className="rajesh-name">Rajesh Sharma</h1>
         <p className="developer">Frontend Developer</p>
-        <p className="web-address"><a href="http://sangamrajsh.me">www.sangamrajsh.me</a></p>
+        <p className="web-address"><a href="http://sangamrajsh.com">www.sangamrajsh.com</a></p>
         </section>
     )
 };
